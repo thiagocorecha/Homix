e5cc3378a3866fd34b56da47a4c3dfe5527679ea
Alex Morega <alex@grep.ro>
2013-04-20 21:09:21 +0000

    read new status format; save to file

--- status.py
+++ status.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
 import sys
+import os
 import re
+from datetime import datetime
+import json
+
+STATE_FILE = os.environ.get('STATE_FILE', '/tmp/arduino_state.json')
 
 
 def read_lines():
@@ -13,16 +18,31 @@ def read_lines():
             yield line.strip()
 
 
-status_pattern = re.compile(r'^\|Temperatura1\:(?P<temp1>\d+\.\d+)\|'
-                            r'\|Temperatura2\:(?P<temp2>\d+)\|'
+status_pattern = re.compile(r'^\|Temperatura\:(?P<temp>\d+)\|'
                             r'\|Light\:(?P<light>\d+)\|$')
 
 
-def main():
+def update_loop():
     for line in read_lines():
         match = status_pattern.match(line)
         if match is not None:
-            print match.groupdict()
+            state = {
+                'temp': int(match.group('temp')),
+                'light': int(match.group('light')),
+                'timestamp': datetime.now().isoformat(),
+            }
+            save_state(state)
+
+
+def save_state(state):
+    temp_state_file = STATE_FILE + '.tmp'
+    with open(temp_state_file, 'wb') as f:
+        json.dump(state, f, indent=2)
+    os.rename(temp_state_file, STATE_FILE)
+
+
+def main():
+    update_loop()
 
 
 if __name__ == '__main__':
